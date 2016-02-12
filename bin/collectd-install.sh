9c947e1bcd43332aac91065c91cf6676cae6a35a
Jim Lindeman <lindj@us.ibm.com>
2016-02-12 13:55:52 -0500

    more collectd install debug

--- bin/collectd-install.sh
+++ bin/collectd-install.sh
@@ -12,9 +12,9 @@ cp $my_dir/collectd*.sh $build_dir
 
 pushd $build_dir
   echo "Unpacking and configuring collectd"
-  tar xzf $my_dir/../collectd.tgz
-  rm $my_dir/../collectd.tgz
-  cp $my_dir/../collectd-related-config/collectd.template etc/collectd.conf
+  tar xvzf $my_dir/../collectd.tgz
+  # rm $my_dir/../collectd.tgz
+  cp $my_dir/../collectd.conf etc/collectd.conf
   ls -l $build_dir
   echo "Finished unpacking and configuring collectd"
 popd
